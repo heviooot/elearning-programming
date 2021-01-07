a33199654edc2372de0cb6efca6dcc56dca8d452
Reinaldo Usadani <reinaldo.bbu20@gmail.com>
2021-01-08 01:39:38 +0800

    minor update

--- src/components/navbar/Dropdown.js
+++ src/components/navbar/Dropdown.js
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 
 const Dropdown = ({toggle, isOpen}) => { // Menangkap state dari isOpen dan function "toggle" dari komponen navbar
   return (
-    <div className={isOpen ? "grid grid-rows-4 right-2 top-16 mt-2 fixed text-center justify-center items-center bg-white rounded shadow-md" : "hidden"} onClick={toggle}>
+    // di div utama, classname akan ditentukan oleh state dari isOpen
+    <div className={isOpen ? "grid grid-rows-4 right-2 top-16 mt-2 fixed overflow-hidden text-center justify-center items-center bg-white rounded shadow-md" : "hidden"}>
       <Link className="p-4 transition duration-200 ease-in-out hover:shadow-md hover:bg-orange hover:text-white">Tentang Kami</Link>
       <Link className="p-4 transition duration-200 ease-in-out hover:shadow-md hover:bg-orange hover:text-white">Contoh Materi</Link>
       <Link className="p-4 transition duration-200 ease-in-out hover:shadow-md hover:bg-orange hover:text-white">Kontak Kami</Link>
