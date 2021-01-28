1b396e1a1e7673946cc276136abcfca0adb18b50
Reinaldo Usadani <reinaldo.bbu20@gmail.com>
2021-01-28 09:10:34 +0800

    Added key props sidebar items

--- src/pages/lessons/Sidebar.js
+++ src/pages/lessons/Sidebar.js
@@ -12,12 +12,12 @@ const Sidebar = ({ lessons, isLoading, error }) => {
       {error && <div>{error}</div>}
       {isLoading && <div>loading...</div>}
       {lessons && lessons.map((lesson) => (
-        <>
-        <h1 key={lesson.id} className="text-lg font-medium my-2">{lesson.topic}</h1>
+        <div key={lesson.id} >
+        <h1 className="text-lg font-medium my-2">{lesson.topic}</h1>
         {lesson.subtopic.map((sub, index) =>(
           <h1 key={index} className="py-2 px-4 text-sm rounded-lg transition duration-200 ease-in-out hover:bg-orange hover:shadow-lg">{sub.title}</h1>
         ))}
-        </>
+        </div>
       ))}
     </div>
   );
