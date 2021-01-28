0ffedb039b2aedd26974dfd7b7fbc5715341dff1
Reinaldo Usadani <reinaldo.bbu20@gmail.com>
2021-01-28 09:07:37 +0800

    Making Sidebar Dynamic

--- src/pages/lessons/Lessons.js
+++ src/pages/lessons/Lessons.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Sidebar from './Sidebar'
+import useFetch from "./../../hooks/useFetch"
 
 const Lessons = () => {
+    const { data, isLoading, error } = useFetch("http://localhost:8000/lessons");
     return (
-        <Sidebar />
+        <Sidebar lessons={data} isLoading={isLoading} error={error} />
         
     )
 }
--- src/pages/lessons/Sidebar.js
+++ src/pages/lessons/Sidebar.js
@@ -1,25 +1,27 @@
 import { Link } from "react-router-dom";
 
-const Sidebar = () => {
+const Sidebar = ({ lessons, isLoading, error }) => {
   return (
     <div className="fixed flex flex-col w-64 h-screen bg-reallydark shadow-md font-roboto z-10 p-4 text-white overflow-y-auto">
-      <Link to="/" className="w-48 font-mono text-3xl text-center rounded-xl mx-auto pb-3 pt-2 px-2 mt-4 mb-6 block bg-orange shadow-lg">
+      <Link
+        to="/"
+        className="w-48 font-mono text-3xl text-center rounded-xl mx-auto pb-3 pt-2 px-2 mt-4 mb-6 block bg-orange shadow-lg"
+      >
         dingoding.
       </Link>
-      <h1 className="text-lg font-medium my-2">Pengenalan</h1>
-      <p className="py-2 px-4 text-sm rounded-lg transition duration-200 ease-in-out hover:bg-orange hover:shadow-lg">Materi 1</p>
-      <p className="py-2 px-4 text-sm rounded-lg transition duration-200 ease-in-out hover:bg-orange hover:shadow-lg">Materi 2</p>
-      <p className="py-2 px-4 text-sm rounded-lg transition duration-200 ease-in-out hover:bg-orange hover:shadow-lg">Materi 3</p>
-      <h1 className="text-lg font-medium my-2">Chapter 1</h1>
-      <p className="py-2 px-4 text-sm rounded-lg transition duration-200 ease-in-out hover:bg-orange hover:shadow-lg">Materi 1</p>
-      <p className="py-2 px-4 text-sm rounded-lg transition duration-200 ease-in-out hover:bg-orange hover:shadow-lg">Materi 2</p>
-      <p className="py-2 px-4 text-sm rounded-lg transition duration-200 ease-in-out hover:bg-orange hover:shadow-lg">Materi 3</p>
-      <h1 className="text-lg font-medium my-2">Chapter 2</h1>
-      <p className="py-2 px-4 text-sm rounded-lg transition duration-200 ease-in-out hover:bg-orange hover:shadow-lg">Materi 1</p>
-      <p className="py-2 px-4 text-sm rounded-lg transition duration-200 ease-in-out hover:bg-orange hover:shadow-lg">Materi 2</p>
-      <p className="py-2 px-4 text-sm rounded-lg transition duration-200 ease-in-out hover:bg-orange hover:shadow-lg">Materi 3</p>
+      {error && <div>{error}</div>}
+      {isLoading && <div>loading...</div>}
+      {lessons && lessons.map((lesson) => (
+        <>
+        <h1 key={lesson.id} className="text-lg font-medium my-2">{lesson.topic}</h1>
+        {lesson.subtopic.map((sub, index) =>(
+          <h1 key={index} className="py-2 px-4 text-sm rounded-lg transition duration-200 ease-in-out hover:bg-orange hover:shadow-lg">{sub.title}</h1>
+        ))}
+        </>
+      ))}
     </div>
   );
 };
 
 export default Sidebar;
+
